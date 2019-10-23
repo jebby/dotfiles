094d7d7c6a40a49fadea97f46bf1b367dfb288b7
Jebby <jebbyjebbyjebby@gmail.com>
2019-10-23 07:02:54 -0500

    Add ipython config

---
+++ ipython/profile_default/startup/00-init.py
@@ -0,0 +1,29 @@
+
+print()
+
+import math
+print("import math")
+
+import os
+print("import os")
+
+import random
+print("import random")
+
+import sys
+print("import sys")
+
+import time
+print("import time")
+
+import numpy as np
+print("import numpy as np")
+
+import requests
+print("import requests")
+
+#  import Xlib.display
+#  print("import Xlib.display")
+#  
+#  import Xlib.X
+#  print("import Xlib.X")
